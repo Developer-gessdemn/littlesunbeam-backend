2efa3a4e1f499f1551d995cf164e3fa0bbf6a7cd
Rithi1325 <rithika13022004@gmail.com>
2026-09-02 14:55:48 +0530

    Updated changes

--- server.js
+++ server.js
@@ -42,7 +42,7 @@ app.use(
 // CORS configuration
 const allowedOrigins = process.env.FRONTEND_URL
   ? process.env.FRONTEND_URL.split(",").map((url) => url.trim())
-  : ["http://localhost:5173", "http://localhost:3000", "http://localhost:8080"];
+  : ["http://localhost:5173", "http://localhost:3000", "https://littlesunbeam.in",];
 
 app.use(
   cors({
